85030d8562de47da3467e8613202ba9628dc345c
y4shiro <y4shironao@gmail.com>
2021-12-11 19:06:18 +0900

    268. If

---
+++ src/easy/268-if.ts
@@ -0,0 +1,14 @@
+/* _____________ ここにコードを記入 _____________ */
+
+type If<C extends boolean, T, F> = C extends true ? T : F;
+
+/* _____________ テストケース _____________ */
+import { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
+  Expect<Equal<If<false, 'a', 2>, 2>>
+];
+
+// @ts-expect-error
+type error = If<null, 'a', 'b'>;
