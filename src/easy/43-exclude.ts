a2ae07ed951efc0badd2ee530e2e8f9ee5c5a3b9
y4shiro <y4shironao@gmail.com>
2021-12-11 18:27:57 +0900

    43. Exclude

---
+++ src/easy/43-exclude.ts
@@ -0,0 +1,22 @@
+/* _____________ ここにコードを記入 _____________ */
+
+type MyExclude<T, U> = T extends U ? never : T;
+
+/* _____________ テストケース _____________ */
+import { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, Exclude<'a' | 'b' | 'c', 'a'>>>,
+  Expect<
+    Equal<
+      MyExclude<'a' | 'b' | 'c', 'a' | 'b'>,
+      Exclude<'a' | 'b' | 'c', 'a' | 'b'>
+    >
+  >,
+  Expect<
+    Equal<
+      MyExclude<string | number | (() => void), Function>,
+      Exclude<string | number | (() => void), Function>
+    >
+  >
+];
