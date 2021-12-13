fcab7080662109b29d4126787c2aa81a7d3b24d1
y4shiro <y4shironao@gmail.com>
2021-12-13 17:49:28 +0900

    20. Promise.all

---
+++ src/medium/20-promise-all.ts
@@ -0,0 +1,20 @@
+/* _____________ ここにコードを記入 _____________ */
+
+declare function PromiseAll<T extends any[]>(
+  values: readonly [...T]
+): Promise<{
+  [P in keyof T]: T[P] extends Promise<infer R> ? R : T[P];
+}>;
+
+/* _____________ テストケース _____________ */
+import { Equal, Expect } from '@type-challenges/utils';
+
+const promiseAllTest1 = PromiseAll([1, 2, 3] as const);
+const promiseAllTest2 = PromiseAll([1, 2, Promise.resolve(3)] as const);
+const promiseAllTest3 = PromiseAll([1, 2, Promise.resolve(3)]);
+
+type cases = [
+  Expect<Equal<typeof promiseAllTest1, Promise<[1, 2, 3]>>>,
+  Expect<Equal<typeof promiseAllTest2, Promise<[1, 2, number]>>>,
+  Expect<Equal<typeof promiseAllTest3, Promise<[number, number, number]>>>
+];
