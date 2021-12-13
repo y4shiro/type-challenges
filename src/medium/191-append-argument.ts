a0b010a1f39a2894954cd10b3661a3c1ef791824
y4shiro <y4shironao@gmail.com>
2021-12-13 19:51:38 +0900

    191. Append Argument

---
+++ src/medium/191-append-argument.ts
@@ -0,0 +1,16 @@
+/* _____________ ここにコードを記入 _____________ */
+
+type AppendArgument<Fn, A> = Fn extends (...args: infer Args) => infer R
+  ? (...args: [...Args, A]) => R
+  : never;
+
+/* _____________ テストケース _____________ */
+import { Equal, Expect } from '@type-challenges/utils';
+
+type Case1 = AppendArgument<(a: number, b: string) => number, boolean>;
+type Result1 = (a: number, b: string, x: boolean) => number;
+
+type Case2 = AppendArgument<() => void, undefined>;
+type Result2 = (x: undefined) => void;
+
+type cases = [Expect<Equal<Case1, Result1>>, Expect<Equal<Case2, Result2>>];
