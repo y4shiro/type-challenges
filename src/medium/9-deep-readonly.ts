5f3c9ad9cb1dab8eb221ee79afbdf8912bd717e1
y4shiro <y4shironao@gmail.com>
2021-12-13 16:30:46 +0900

    9. Deep Readonly

---
+++ src/medium/9-deep-readonly.ts
@@ -0,0 +1,46 @@
+/* _____________ ここにコードを記入 _____________ */
+
+type DeepReadonly<T> = {
+  readonly [P in keyof T]: T[P] extends Record<string, unknown>
+    ? DeepReadonly<T[P]>
+    : T[P];
+};
+
+/* _____________ テストケース _____________ */
+import { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [Expect<Equal<DeepReadonly<X>, Expected>>];
+
+type X = {
+  a: () => 22;
+  b: string;
+  c: {
+    d: boolean;
+    e: {
+      g: {
+        h: {
+          i: true;
+          j: 'string';
+        };
+        k: 'hello';
+      };
+    };
+  };
+};
+
+type Expected = {
+  readonly a: () => 22;
+  readonly b: string;
+  readonly c: {
+    readonly d: boolean;
+    readonly e: {
+      readonly g: {
+        readonly h: {
+          readonly i: true;
+          readonly j: 'string';
+        };
+        readonly k: 'hello';
+      };
+    };
+  };
+};
