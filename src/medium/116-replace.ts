39e5d5be378f07c6f859d64fa99a0c5e59819c58
y4shiro <y4shironao@gmail.com>
2021-12-13 19:27:13 +0900

    116. Replace

---
+++ src/medium/116-replace.ts
@@ -0,0 +1,22 @@
+/* _____________ ここにコードを記入 _____________ */
+
+type Replace<
+  S extends string,
+  From extends string,
+  To extends string
+> = From extends ''
+  ? S
+  : S extends `${infer L}${From}${infer R}`
+  ? `${L}${To}${R}`
+  : S;
+
+/* _____________ テストケース _____________ */
+import { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<Replace<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<Replace<'foobarbar', 'bar', 'foo'>, 'foofoobar'>>,
+  Expect<Equal<Replace<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bra', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'', '', ''>, ''>>
+];
