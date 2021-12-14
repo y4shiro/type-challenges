7f3324601bda3a0ac9ba16b2ecf5c12a35842dd1
y4shiro <y4shironao@gmail.com>
2021-12-14 19:20:53 +0900

    1042. IsNever

---
+++ src/medium/1042-isnever.ts
@@ -0,0 +1,16 @@
+/* _____________ Your Code Here _____________ */
+
+type IsNever<T> = [T] extends [never] ? true : false;
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<IsNever<never>, true>>,
+  Expect<Equal<IsNever<never | string>, false>>,
+  Expect<Equal<IsNever<''>, false>>,
+  Expect<Equal<IsNever<undefined>, false>>,
+  Expect<Equal<IsNever<null>, false>>,
+  Expect<Equal<IsNever<[]>, false>>,
+  Expect<Equal<IsNever<{}>, false>>
+];
