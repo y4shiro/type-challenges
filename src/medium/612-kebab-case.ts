3bc95e46b6dfc239ed3adb89880f71fe2f261504
y4shiro <y4shironao@gmail.com>
2021-12-14 18:24:00 +0900

    612. KebabCase

---
+++ src/medium/612-kebab-case.ts
@@ -0,0 +1,22 @@
+/* _____________ Your Code Here _____________ */
+
+type KebabCase<S extends string> = S extends `${infer F}${infer R}`
+  ? R extends Uncapitalize<R>
+    ? `${Lowercase<F>}${KebabCase<R>}`
+    : `${Lowercase<F>}-${KebabCase<Uncapitalize<R>>}`
+  : S;
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<KebabCase<'FooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'fooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'foo-bar'>, 'foo-bar'>>,
+  Expect<Equal<KebabCase<'foo_bar'>, 'foo_bar'>>,
+  Expect<Equal<KebabCase<'Foo-Bar'>, 'foo--bar'>>,
+  Expect<Equal<KebabCase<'ABC'>, 'a-b-c'>>,
+  Expect<Equal<KebabCase<'-'>, '-'>>,
+  Expect<Equal<KebabCase<''>, ''>>,
+  Expect<Equal<KebabCase<'😎'>, '😎'>>
+];
