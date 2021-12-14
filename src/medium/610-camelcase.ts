6d5c93a6e16933265767a5463a045d2ee189f835
y4shiro <y4shironao@gmail.com>
2021-12-14 18:12:22 +0900

    615. CamelCase

---
+++ src/medium/610-camelcase.ts
@@ -0,0 +1,25 @@
+/* _____________ Your Code Here _____________ */
+
+type CamelCase<S extends string> = S extends `${infer L}-${infer R}`
+  ? R extends Capitalize<R>
+    ? `${L}-${CamelCase<R>}`
+    : `${L}${CamelCase<Capitalize<R>>}`
+  : S;
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<CamelCase<'foo-bar-baz'>, 'fooBarBaz'>>,
+  Expect<Equal<CamelCase<'foo-Bar-Baz'>, 'foo-Bar-Baz'>>,
+  Expect<Equal<CamelCase<'foo-Bar-baz'>, 'foo-BarBaz'>>,
+  Expect<Equal<CamelCase<'foo-bar'>, 'fooBar'>>,
+  Expect<Equal<CamelCase<'foo_bar'>, 'foo_bar'>>,
+  Expect<Equal<CamelCase<'foo--bar----baz'>, 'foo-Bar---Baz'>>,
+  Expect<Equal<CamelCase<'a-b-c'>, 'aBC'>>,
+  Expect<Equal<CamelCase<'a-b-c-'>, 'aBC-'>>,
+  Expect<Equal<CamelCase<'ABC'>, 'ABC'>>,
+  Expect<Equal<CamelCase<'-'>, '-'>>,
+  Expect<Equal<CamelCase<''>, ''>>,
+  Expect<Equal<CamelCase<'😎'>, '😎'>>
+];
