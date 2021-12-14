63e495a8a5d6407826aa6baf78b58ee1824bb188
y4shiro <y4shironao@gmail.com>
2021-12-14 11:10:09 +0900

    296. Permutation

---
+++ src/medium/296-permutaiton.ts
@@ -0,0 +1,37 @@
+/* _____________ ここにコードを記入 _____________ */
+
+type Permutation<T, K = T> = [T] extends [never]
+  ? []
+  : K extends K
+  ? [K, ...Permutation<Exclude<T, K>>]
+  : never;
+
+/* _____________ テストケース _____________ */
+import { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<Permutation<'A'>, ['A']>>,
+  Expect<
+    Equal<
+      Permutation<'A' | 'B' | 'C'>,
+      | ['A', 'B', 'C']
+      | ['A', 'C', 'B']
+      | ['B', 'A', 'C']
+      | ['B', 'C', 'A']
+      | ['C', 'A', 'B']
+      | ['C', 'B', 'A']
+    >
+  >,
+  Expect<
+    Equal<
+      Permutation<'B' | 'A' | 'C'>,
+      | ['A', 'B', 'C']
+      | ['A', 'C', 'B']
+      | ['B', 'A', 'C']
+      | ['B', 'C', 'A']
+      | ['C', 'A', 'B']
+      | ['C', 'B', 'A']
+    >
+  >,
+  Expect<Equal<Permutation<never>, []>>
+];
